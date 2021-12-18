eca3f7c1f6da9464ed8a896063cdea8138aef3e2
Infarh <shmachilin@yandex.ru>
2021-12-18 23:02:38 +0300

    Больше модульных тестов

--- MathCore.DI/InjectAttribute.cs
+++ MathCore.DI/InjectAttribute.cs
@@ -3,9 +3,10 @@
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method | AttributeTargets.Parameter)]
 public sealed class InjectAttribute : Attribute
 {
-    public bool? Required { get; set; }
+    public bool Required { get; set; } = true;
 
     public InjectAttribute() { }
 
-    public InjectAttribute(bool? Required) => this.Required = Required;
+    public InjectAttribute(bool Required) => this.Required = Required;
+
 }
--- Tests/MathCore.DI.Tests/ComplexInjectionRegistratorTests.cs
+++ Tests/MathCore.DI.Tests/ComplexInjectionRegistratorTests.cs
@@ -1,26 +1,17 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MathCore.DI.Tests;
 
 [TestClass]
 public class ComplexInjectionRegistratorTests
 {
-    private class TestClass
-    {
-        private readonly string _Name;
-        private readonly string? _Value;
-
-        public TestClass(string Name, string? Value)
-        {
-            _Name = Name;
-            _Value = Value;
-        }
-    }
-
+    /// <summary>Первый тестовый сервис</summary>
     private interface ITestService1 { }
 
+    /// <summary>Простая реализация тестового сервиса - пустой класс</summary>
     private class SimpleTestService1 : ITestService1 { }
 
+    /// <summary>Проверка регистрации сервиса, подразумевающая стандартное поведение контейнера</summary>
     [TestMethod]
     public void SimpleRegistration()
     {
@@ -47,18 +38,24 @@ public class ComplexInjectionRegistratorTests
         Assert.That.Value(instance).Is(implementation_type);
     }
 
+    /// <summary>Второй тестовый сервис</summary>
     private interface ITestService2 { }
 
+    /// <summary>Второй тестовый сервис, реализующий внедрение через поле</summary>
     private class Service2WithPrivateFieldOfService1 : ITestService2
     {
+        /// <summary>Поле, в которое должно произойти внедрение экземпляра сервиса</summary>
         [Inject]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Добавить модификатор только для чтения")]
         // ReSharper disable once FieldCanBeMadeReadOnly.Local
         private ITestService1 _Service1 = null!;
-
+        
+        /// <summary>Метод, предназначенный для извлечения значения поля для проверки факта внедрения</summary>
+        /// <returns>Значение поля <see cref="_Service1"/></returns>
         public ITestService1 GetFieldValue() => _Service1;
     }
 
+    /// <summary>Проверка возможности осуществления внедрения сервиса в приватное поле, помеченное атрибутом <see cref="InjectAttribute"/></summary>
     [TestMethod]
     public void InjectionWithFieldsPrivate()
     {
@@ -92,6 +89,7 @@ public class ComplexInjectionRegistratorTests
         Assert.That.Value(field_value).IsEqual(service1_instance);
     }
 
+    /// <summary>Тестовая реализация сервиса со скрытым полем тольео для чтения, кда требуется выполнить внедрение свойства</summary>
     private class Service2WithPrivateReadonlyFieldOfService1 : ITestService2
     {
         [Inject]
@@ -100,6 +98,7 @@ public class ComplexInjectionRegistratorTests
         public ITestService1 GetFieldValue() => _Service1;
     }
 
+    /// <summary>Тест внедрения в <c>private readonly</c> поле</summary>
     [TestMethod]
     public void InjectionWithFieldsPrivateReadonly()
     {
@@ -133,6 +132,7 @@ public class ComplexInjectionRegistratorTests
         Assert.That.Value(field_value).IsEqual(service1_instance);
     }
 
+    /// <summary>Тестовый метод с приватным свойством для чтения и для записи</summary>
     private class Service2WithPrivatePropertyOfService1 : ITestService2
     {
         [Inject]
@@ -141,6 +141,7 @@ public class ComplexInjectionRegistratorTests
         public ITestService1 GetPropertyValue() => Service1;
     }
 
+    /// <summary>Тестирование внедрения в приватное свойство с доступом для чтения и для записи</summary>
     [TestMethod]
     public void InjectionWithPropertyPrivate()
     {
@@ -174,6 +175,7 @@ public class ComplexInjectionRegistratorTests
         Assert.That.Value(field_value).IsEqual(service1_instance);
     }
 
+    /// <summary>Тестовый сервис с приватным свойством с доступом для чтения и для инициализации</summary>
     private class Service2WithPrivateInitPropertyOfService1 : ITestService2
     {
         [Inject]
@@ -182,6 +184,7 @@ public class ComplexInjectionRegistratorTests
         public ITestService1 GetPropertyValue() => Service1;
     }
 
+    /// <summary>Тестирование внедрения в приватное свойство с доступом для чтения и инициализации</summary>
     [TestMethod]
     public void InjectionWithPropertyInitPrivate()
     {
@@ -215,10 +218,13 @@ public class ComplexInjectionRegistratorTests
         Assert.That.Value(field_value).IsEqual(service1_instance);
     }
 
+    /// <summary>Тестовый сервис 3</summary>
     private interface ITestService3 { }
 
+    /// <summary>Простая реализация тестового сервиса 3</summary>
     private class SimpleTestService3 : ITestService3 { }
 
+    /// <summary>Реализация сервиса 2 с внедрением в приватное поле и конструктор</summary>
     private class Service2WithConstructorParameterOfService1AndFieldOfService3 : ITestService2
     {
         private readonly ITestService1 _Service1;
@@ -232,6 +238,7 @@ public class ComplexInjectionRegistratorTests
         public ITestService3 GetService3Value() => _Service3;
     }
 
+    /// <summary>Тестирование внедрение в приватное поле и конструктор одновременно</summary>
     [TestMethod]
     public void InjectionWithConstructorAndPropertyParameter()
     {
@@ -269,15 +276,17 @@ public class ComplexInjectionRegistratorTests
         Assert.That.Value(field_value_service3).IsEqual(service3_instance);
     }
 
+    /// <summary>Тестовая реализация сервиса с выполнением внедрения в конструктор с управленем атрибутом <see cref="InjectAttribute"/></summary>
     private class Service2WithConstructorParameterInjectAttribute : ITestService2
     {
-        private ITestService1 _Service1;
+        private readonly ITestService1 _Service1;
 
         public Service2WithConstructorParameterInjectAttribute([Inject] ITestService1 Service1) => _Service1 = Service1;
 
         public ITestService1 GetService1Value() => _Service1;
     }
 
+    /// <summary>Тестирование управляемого внедрения в конструктор с помощью атрибута <see cref="InjectAttribute"/></summary>
     [TestMethod]
     public void InjectionWithConstructorParameterInjectAttribute()
     {
@@ -309,5 +318,138 @@ public class ComplexInjectionRegistratorTests
         var field_value_service1 = service2_instance.ActualValue.GetService1Value();
 
         Assert.That.Value(field_value_service1).IsEqual(service1_instance);
+    }
+
+    /// <summary>Тестовая реализация сервиса 2 с опциональным параметром в конструкторе</summary>
+    private class Service2WithOptionalConstructorParameter : ITestService2
+    {
+        private readonly ITestService1 _Service1;
+
+        public Service2WithOptionalConstructorParameter([Inject(false)] ITestService1 Service1) => _Service1 = Service1;
+
+        public ITestService1 GetService1() => _Service1;
+    }
+
+    /// <summary>Тестирование сервиса с опциональным параметром в конструкторе</summary>
+    [TestMethod]
+    public void InjectWithOptionalConstructorParameter()
+    {
+        var service_collection_with_service1 = new ServiceCollection();
+        var service_collection_without_service1 = new ServiceCollection();
+
+        service_collection_with_service1.AddSingleton<ITestService1>(new SimpleTestService1());
+
+        var service1_interface = typeof(ITestService2);
+        var service2_implementation = typeof(Service2WithOptionalConstructorParameter);
+        service_collection_with_service1.AddService(service1_interface, service2_implementation, ServiceLifetime.Transient);
+        service_collection_without_service1.AddService(service1_interface, service2_implementation, ServiceLifetime.Transient);
+
+        var services_with_service1 = service_collection_with_service1.BuildServiceProvider();
+        var services_without_service1 = service_collection_without_service1.BuildServiceProvider();
+
+        var service1_1 = services_with_service1.GetService<ITestService1>();
+        var service1_2 = services_without_service1.GetService<ITestService1>();
+
+        Assert.That.Value(service1_1).IsNotNull();
+        Assert.That.Value(service1_2).IsNull();
+
+        var service2_1 = services_with_service1.GetRequiredService<ITestService2>();
+        var service2_2 = services_without_service1.GetRequiredService<ITestService2>();
+
+        Assert.That.Value(service2_1)
+           .As<Service2WithOptionalConstructorParameter>()
+           .Where(s => s.GetService1()).Check(s1 => s1.IsNotNull());
+
+        Assert.That.Value(service2_2)
+           .As<Service2WithOptionalConstructorParameter>()
+           .Where(s => s.GetService1()).Check(s1 => s1.IsNull());
+    }
+
+    /// <summary>Тестовая реализация сервиса 2 с опциональным полем</summary>
+    private class Service2WithOptionalPrivateReadonlyField : ITestService2
+    {
+        [Inject(false)]
+        private readonly ITestService1? _Service1;
+
+        public ITestService1? GetService1() => _Service1;
+    }
+
+    /// <summary>Тестирование возможности внедрения сервиса в опциональное поле при отсутствии сервиса в контейнере</summary>
+    [TestMethod]
+    public void InjectWithOptionalField()
+    {
+        var service_collection_with_service1 = new ServiceCollection();
+        var service_collection_without_service1 = new ServiceCollection();
+
+        service_collection_with_service1.AddSingleton<ITestService1>(new SimpleTestService1());
+
+        var service1_interface = typeof(ITestService2);
+        var service2_implementation = typeof(Service2WithOptionalPrivateReadonlyField);
+        service_collection_with_service1.AddService(service1_interface, service2_implementation, ServiceLifetime.Transient);
+        service_collection_without_service1.AddService(service1_interface, service2_implementation, ServiceLifetime.Transient);
+
+        var services_with_service1 = service_collection_with_service1.BuildServiceProvider();
+        var services_without_service1 = service_collection_without_service1.BuildServiceProvider();
+
+        var service1_1 = services_with_service1.GetService<ITestService1>();
+        var service1_2 = services_without_service1.GetService<ITestService1>();
+
+        Assert.That.Value(service1_1).IsNotNull();
+        Assert.That.Value(service1_2).IsNull();
+
+        var service2_1 = services_with_service1.GetRequiredService<ITestService2>();
+        var service2_2 = services_without_service1.GetRequiredService<ITestService2>();
+
+        Assert.That.Value(service2_1)
+           .As<Service2WithOptionalPrivateReadonlyField>()
+           .Where(s => s.GetService1()).Check(s1 => s1.IsNotNull());
+
+        Assert.That.Value(service2_2)
+           .As<Service2WithOptionalPrivateReadonlyField>()
+           .Where(s => s.GetService1()).Check(s1 => s1.IsNull());
+    }
+
+    /// <summary>Тестовая реализация сервиса 2 с опциональным свойством</summary>
+    private class Service2WithOptionalPrivateReadonlyProperty : ITestService2
+    {
+        [Inject(false)]
+        private ITestService1? Service1 { get; init; }
+
+        public ITestService1? GetService1() => Service1;
+    }
+
+    /// <summary>Тестирование возможности внедрения сервиса в опциональное свойство при отсутствии сервиса в контейнере</summary>
+    [TestMethod]
+    public void InjectWithOptionalProperty()
+    {
+        var service_collection_with_service1 = new ServiceCollection();
+        var service_collection_without_service1 = new ServiceCollection();
+
+        service_collection_with_service1.AddSingleton<ITestService1>(new SimpleTestService1());
+
+        var service1_interface = typeof(ITestService2);
+        var service2_implementation = typeof(Service2WithOptionalPrivateReadonlyProperty);
+        service_collection_with_service1.AddService(service1_interface, service2_implementation, ServiceLifetime.Transient);
+        service_collection_without_service1.AddService(service1_interface, service2_implementation, ServiceLifetime.Transient);
+
+        var services_with_service1 = service_collection_with_service1.BuildServiceProvider();
+        var services_without_service1 = service_collection_without_service1.BuildServiceProvider();
+
+        var service1_1 = services_with_service1.GetService<ITestService1>();
+        var service1_2 = services_without_service1.GetService<ITestService1>();
+
+        Assert.That.Value(service1_1).IsNotNull();
+        Assert.That.Value(service1_2).IsNull();
+
+        var service2_1 = services_with_service1.GetRequiredService<ITestService2>();
+        var service2_2 = services_without_service1.GetRequiredService<ITestService2>();
+
+        Assert.That.Value(service2_1)
+           .As<Service2WithOptionalPrivateReadonlyProperty>()
+           .Where(s => s.GetService1()).Check(s1 => s1.IsNotNull());
+
+        Assert.That.Value(service2_2)
+           .As<Service2WithOptionalPrivateReadonlyProperty>()
+           .Where(s => s.GetService1()).Check(s1 => s1.IsNull());
     }
 }
