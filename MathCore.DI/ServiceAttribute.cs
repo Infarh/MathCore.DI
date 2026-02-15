f8117e762eea6db5277d933233fdd90c5406785f
Infarh <shmachilin@gmail.com>
2026-02-15 22:05:39 +0300

    Рефакторинг

--- MathCore.DI/ServiceAttribute.cs
+++ MathCore.DI/ServiceAttribute.cs
@@ -1,19 +1,13 @@
 ﻿namespace MathCore.DI;
 
-/// <summary>
-/// Атрибут, используемый для маркировки класса или интерфейса в качестве сервиса.
-/// </summary>
+/// <summary>Атрибут, используемый для маркировки класса или интерфейса в качестве сервиса</summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, Inherited = false)]
 public class ServiceAttribute : Attribute
 {
-    /// <summary>
-    /// Получает или устанавливает режим временного lifetime сервиса.
-    /// </summary>
+    /// <summary>Получает или устанавливает режим временного lifetime сервиса</summary>
     public ServiceLifetime Mode { get; set; } = ServiceLifetime.Transient;
 
-    /// <summary>
-    /// Получает или устанавливает тип реализации сервиса.
-    /// </summary>
+    /// <summary>Получает или устанавливает тип реализации сервиса</summary>
     /// <exception cref="InvalidOperationException">Вызывается, если тип реализации устанавливается после установки интерфейса.</exception>
     public Type? Implementation
     {
@@ -27,9 +21,7 @@ public class ServiceAttribute : Attribute
         }
     }
 
-    /// <summary>
-    /// Получает или устанавливает тип интерфейса сервиса.
-    /// </summary>
+    /// <summary>Получает или устанавливает тип интерфейса сервиса</summary>
     /// <exception cref="InvalidOperationException">Вызывается, если тип интерфейса устанавливается после установки реализации.</exception>
     public Type? Interface
     {
@@ -43,20 +35,14 @@ public class ServiceAttribute : Attribute
         }
     }
 
-    /// <summary>
-    /// Инициализирует новый экземпляр класса <see cref="ServiceAttribute"/>.
-    /// </summary>
+    /// <summary>Инициализирует новый экземпляр класса <see cref="ServiceAttribute"/></summary>
     public ServiceAttribute() { }
 
-    /// <summary>
-    /// Инициализирует новый экземпляр класса <see cref="ServiceAttribute"/> с указанным режимом временного lifetime.
-    /// </summary>
+    /// <summary>Инициализирует новый экземпляр класса <see cref="ServiceAttribute"/> с указанным режимом временного lifetime</summary>
     /// <param name="Mode">Режим временного lifetime сервиса.</param>
     public ServiceAttribute(ServiceLifetime Mode) => this.Mode = Mode;
 
-    /// <summary>
-    /// Деконструирует атрибут сервиса в его части.
-    /// </summary>
+    /// <summary>Деконструирует атрибут сервиса в его части</summary>
     /// <param name="implementation">Тип реализации сервиса.</param>
     /// <param name="mode">Режим временного lifetime сервиса.</param>
     public void Deconstruct(out Type? implementation, out ServiceLifetime mode)
@@ -65,9 +51,7 @@ public class ServiceAttribute : Attribute
         mode = Mode;
     }
 
-    /// <summary>
-    /// Деконструирует атрибут сервиса в его части.
-    /// </summary>
+    /// <summary>Деконструирует атрибут сервиса в его части</summary>
     /// <param name="service">Тип интерфейса сервиса.</param>
     /// <param name="implementation">Тип реализации сервиса.</param>
     /// <param name="mode">Режим временного lifetime сервиса.</param>
